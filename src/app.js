394cd867ca1fa0abbfb4c2f3fea3453afa3732ed
wesleyMirand <wewesantos@hotmail.com>
2020-05-15 14:36:44 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -11,11 +11,23 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -23,11 +35,21 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  // TODO()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository.id === id);
+
+  if(!repository){
+    return response.status(400).send();
+  }
+
+  repository.likes += 1
+
+  return response.json(repository);
 });
 
-module.exports = app;
+module.exports = app; 
